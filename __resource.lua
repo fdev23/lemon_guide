1855cc54740278e29ebe6747356ceadada0a3692
Loupass99 <91392785+Loupass99@users.noreply.github.com>
2021-09-25 23:00:49 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,29 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script 'client/client.lua'
+
+ui_page 'HUD/ui.html'
+loadscreen 'LoadingScreen/ui.html'
+
+files {
+  'config.js',
+  'LoadingScreen/ui.html',
+  'HUD/ui.html',
+  'assets/css/style.css',
+  'assets/img/cursor.png',
+  'assets/img/discord.png',
+  'assets/img/home.png',
+  'assets/img/bg.png',
+  'assets/img/logo.png',
+  'assets/img/bg.jpg',
+  'assets/js/app.js',
+  'assets/audio/song.mp3',
+}
+
+
+
+
+
+
+
+
+
---
+++ config.js
@@ -0,0 +1,94 @@
+var config = {
+  'title': 'BENVENUTO SU LEMONCITY',
+  'welcomeMessage': '',
+  // Adjust music volume 0-1
+  'volume': '0.10',
+  // Change Discord settings and link.
+  'discord': {
+    'show': true,
+    'discordLink': 'discord.gg/lemoncityrp',
+  },
+  // Change which key you have set in GuideHud's client.
+  'menuHotkey': 'U',
+  // Turn on/off rule tabs. true/false
+  'rules': {
+    'generalconduct': true,
+    'roleplaying': true,
+    'rdmvdm': true,
+    'metagaming': true,
+    'newlife': true,
+    'abuse': true,
+  },
+}
+
+// Home page annountments.
+var announcements = [
+  '',
+]
+
+
+// Add/Modify rules below.
+var guidelines = [
+  'Violare una di queste regole recherà seri provvedimenti.',
+  'Se vedi qualcuno violare le regole, segnalalo.',
+  'Lo staff si riserva la possibilità di bannare player tossici/troller.',
+  'Non tutte le regole sono scritte, usa un pò di buon senso.',
+  'Tutte le regole sono disponibili nel nostro discord.',
+]
+
+var generalconduct = [
+  'Razzismo e tossicità non sono consentiti.',
+  'I player non possono ruolare stupri e violenze sessuali.',
+]
+
+var roleplaying = [
+  'I giocatori devono ruolare qualsiasi situazione, sempre.',
+  'I giocatori devono sempre ruolare i ferimenti.',
+  'Vietato uscire fuori RP e spammare in ooc.',
+]
+
+var rdmvdm = [
+  'Il giocatore non può uccidere senza un motivo rp.',
+  'Vietato investire altri giocatori in qualsiasi caso, per ucciderli o mandarli in coma.',
+]
+
+var metagaming = [
+  'Non è tollerato usare informazioni fuori dal gioco, in rp',
+
+]
+
+var newlife = [
+  'il permadeath deve essere assistito da almeno 2 admin.',
+  'Una volta ruolato, se accettato, il player dimenticherà la sua vecchia vita e le sue vecchie conoscenze.',
+  'Il player non potrà avere le stesse sembianze e lo stesso passato dello scorso personaggio',
+]
+
+var abuse = [
+  'Vietato abusare di bug',
+  'Vietato uso di cheat in qualsiasi caso.',
+  'Segnala sempre uso di cheat.',
+]
+
+// Modify hotkeys below.
+var generalhotkeys = [
+  'Premi <kbd>K</kbd> per aprire il cellulare',
+  'Premi <kbd>X</kbd> per alzare le mani',
+  'Press <kbd>F10</kbd> per vedere la lista player',
+  'Press <kbd>B</kbd> per indicare.',
+  'Press <kbd>F2</kbd> per aprire inventario',
+  'La lista di comandi approfondita la trovi sul nostro discord',
+]
+
+var rphotkeys = [
+]
+
+var vehiclehotkeys = [
+  'Premi <kbd>L</kbd> per chiudere il veicolo',
+  'Premi <kbd>G</kbd> per mettere la cintura',
+   'La lista di comandi approfondita la trovi sul nostro discord',
+]
+
+var jobshotkeys = [
+  'Premi <kbd>F6</kbd> per aprire il menu lavoro',
+   'La lista di comandi approfondita la trovi sul nostro discord',
+]
